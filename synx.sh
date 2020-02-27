5a8ac2f63f709248612b187d23efe4e0278a469e
Nikita Konopelko <nik.sativa@gmail.com>
2020-02-27 14:49:32 +0200

    no message

--- synx.sh
+++ synx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for f in $(find . -name "*.xcodeproj" -not -path "./.bundle/*" -not -path "./Pods/*" -not -path "./bin/*" -not -path "./Build/" -not -path "./DrivedData/*"); do
+for f in $(find . -name "*.xcodeproj" -not -path "./.bundle/*" -not -path "./Pods/*" -not -path "./bin/*" -not -path "./Build/" -not -path "./DrivedData/*" -not -path "./vendor/*"); do
 
 case $(basename $f) in
 Pods.xcodeproj|Sample.xcodeproj|dummy.xcodeproj)
