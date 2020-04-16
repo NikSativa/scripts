1879b1312f03e1bf357be89606af31445d93bc73
Nikita Konopelko <nik.sativa@gmail.com>
2020-04-16 20:32:48 +0300

    no message

---
+++ restart.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+script_full_path=$(dirname "$0")
+
+echo "====> killing Xcode"
+killall Xcode
+sleep 1
+
+echo "====> removing All"
+sh $script_full_path/rm/all.sh
+
+echo "====> installing bundle"
+sh $script_full_path/bundle/install.sh
+
+echo "====> installing pods"
+sh $script_full_path/pod/install.sh
+
+echo "====> opening Xcode"
+sleep 1
+xed .
+
+say jobs done
