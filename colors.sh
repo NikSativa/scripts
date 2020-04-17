99e8d15985eae080e2b4e7f679b28a532db6b84b
Nikita Konopelko <nik.sativa@gmail.com>
2020-04-17 11:07:18 +0300

    no message

--- colors.sh
+++ colors.sh
@@ -5,55 +5,15 @@
 # source $(dirname "$0")/colors.sh
 # 2. add colors like this
 # echo "${GREEN}<your text is here>${NORMAL}"
-# or
-# GREEN(<your text is here>)
 
-BOLD_STYLE=$(tput bold)
-BOLD_TEXT() {
-    return ${BOLD_STYLE}$1${BOLD_STYLE}
-}
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
 
-NORMAL_STYLE=$(tput sgr0)
-NORMAL_TEXT() {
-    return ${NORMAL_STYLE}$1${NORMAL_STYLE}
-}
-
-BLACK_COLOR=$(tput setaf 0)
-BLACK() {
-    echo ${BLACK_COLOR}$1${NORMAL_STYLE}
-}
-
-RED_COLOR=$(tput setaf 1)
-RED() {
-    echo ${RED_COLOR}$1${NORMAL_STYLE}
-}
-
-GREEN_COLOR=$(tput setaf 2)
-GREEN() {
-    echo ${GREEN_COLOR}$1${NORMAL_STYLE}
-}
-
-YELLOW_COLOR=$(tput setaf 2)
-YELLOW() {
-    echo ${YELLOW_COLOR}$1${NORMAL_STYLE}
-}
-
-BLUE_COLOR=$(tput setaf 4)
-BLUE() {
-    echo ${BLUE_COLOR}$1${NORMAL_STYLE}
-}
-
-MAGENTA_COLOR=$(tput setaf 5)
-MAGENTA() {
-    echo ${MAGENTA_COLOR}$1${NORMAL_STYLE}
-}
-
-CYAN_COLOR=$(tput setaf 6)
-CYAN() {
-    echo ${CYAN_COLOR}$1${NORMAL_STYLE}
-}
-
-WHITE_COLOR=$(tput setaf 7)
-WHITE() {
-    echo ${WHITE_COLOR}$1${NORMAL_STYLE}
-}
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
