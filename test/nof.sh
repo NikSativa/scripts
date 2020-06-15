4ea08908f724e5cf5a2c3e1b63e64d91ab43f85e
Nikita Konopelko <nik.sativa@gmail.com>
2020-06-15 18:56:25 +0300

    fix nof

--- test/nof.sh
+++ test/nof.sh
@@ -1,61 +1,27 @@
 #!/bin/sh
+source $(dirname "$0")/../colors.sh
 
-## Don't commit focused tests.
-## Use this as a pre-commit hook and the commit won't succeed if you have staged changes that contain `fdescribe`, `fcontext`, `fit`, `fspecify` or `fexample`.
-## Copy this file as `pre-commit` into the `.git/hooks` folder of your repository (create it if neccessary) and chmod +x the file.
+DIR=$(git rev-parse --show-cdup)
 
-isForHook=false
-for i in "$@"
+declare -a RESULT=()
+for FILE in $(find $DIR -regex ".*\.swift")
 do
-
-case $i in
-    -h=*|-hook=*)
-        case "${i#*=}" in
-            1|true|yes)
-                isForHook=true
-            ;;
-
-            0|false|no)
-                isForHook=false
-            ;;
-
-            *) # unknown option
-            ;;
-        esac
-    ;;
-    *) # unknown option
-    ;;
-    esac
+   if [[ ! $FILE == */Pods/* ]]
+   then
+      if grep -q -e '^ *fdescribe(' -e '^ *fcontext(' -e '^ *fit(' -- "$FILE"
+      then
+         RESULT+=($(basename "$FILE"))
+      fi
+   fi
 done
 
-if $isForHook
+if [ ${#RESULT[@]} == 0 ]
 then
-    cached="--cached"
-    if git rev-parse --verify HEAD >/dev/null 2>&1
-    then
-        against=HEAD
-    else
-        # Initial commit: diff against an empty tree object
-        against=$(git hash-object -t tree /dev/null)
-    fi
+   echo "${GREEN}No focuses to remove${NORMAL}"
 else
-    cached=``
-    against=$(git hash-object -t tree /dev/null)
+   for FILE in ${RESULT[@]}
+   do
+      echo "${RED}$FILE${NORMAL}"
+   done
+   echo "${RED}${BOLD}do not commit fdescribe, fcontext, or fit${NORMAL}"
 fi
-
-SAVEIFS=$IFS
-IFS=$'\n'
-for FILE in `git diff-index --diff-filter=AM --name-only $against`
-do
-    # Check if the file contains patterns
-    if git grep $cached -q -e '^ *fdescribe' -e '^ *fcontext' -e '^ *fit' -- $FILE
-    then
-        echo $FILE  ': Commit blocked to prevent commiting test focus. (do not commit fdescribe, fcontext, or fit)'
-        exit 1
-    fi
-done
-
-IFS=$SAVEIFS
-echo "Commit ok, no focused tests found in staged changes"
-
-exit
