b16dfdd70f61b4b465cceb16f9596568f63b64bc
Nikita Konopelko <nik.sativa@gmail.com>
2020-10-14 09:33:59 +0300

    improvements of file search

--- test/nof.sh
+++ test/nof.sh
@@ -6,61 +6,69 @@ fromCurrentFolder=true
 
 for i in "$@"
 do
-  case $i in
-      -s|-silent)
-          silent=true
-      ;;
-      -r|-root)
-          fromCurrentFolder=false
-      ;;
-  esac
+	case $i in
+		-s|-silent)
+		  silent=true
+		;;
+		-r|-root)
+		  fromCurrentFolder=false
+		;;
+	esac
 done
 
 if $fromCurrentFolder
 then
-  DIR="./"
+	DIR="./"
 else
-  DIR=$(git rev-parse --show-cdup)
+	DIR=$(git rev-parse --show-cdup)
 fi
 
 declare -a RESULT=()
-declare -a FILES=$(find $DIR -type f -regex ".*\.swift")
+declare -a FILES=$(find . -name "*.swift" \
+					-not -path "**/.bundle/**" \
+					-not -path "**/Pods/**" \
+					-not -path "**/.bin/**" \
+					-not -path "**/Build/**" \
+					-not -path "**/.build/**" \
+					-not -path "**/DrivedData/**" \
+					-not -path "**/fastlane/**" \
+					-not -path "**/vendor/**" \
+					-not -path "**/Quick/**" \
+					-not -path "**+Quick**" \
+					-not -path "**Quick+**")
 for FILE in ${FILES[@]}
 do
-   if [[ ! $FILE == */Pods/* ]] && [[ ! $FILE == **Quick+** ]] && [[ ! $FILE == **/Quick/** ]] && [[ ! $FILE == *+Quick+** ]]
-   then
-      if grep -q -e '^ *fdescribe(' -e '^ *fcontext(' -e '^ *fit(' -- "$FILE"
-      then
-         RESULT+=($FILE)
-      fi
-   fi
+	if grep -q -e '^ *fdescribe(' -e '^ *fcontext(' -e '^ *fit(' -- "$FILE"
+	then
+		RESULT+=($FILE)
+	fi
 done
 
 if [ ${#RESULT[@]} == 0 ]
 then
-  echo "${GREEN}No focuses to remove${NORMAL}"
-  exit 0
+	echo "${GREEN}No focuses to remove${NORMAL}"
+	exit 0
 fi
 
 if $silent
 then
-  for FILE in ${RESULT[@]}
-  do
-    FILE=$(basename "$FILE")
-    echo "${RED}$FILE${NORMAL}"
-  done
+	for FILE in ${RESULT[@]}
+	do
+		FILE=$(basename "$FILE")
+		echo "${RED}$FILE${NORMAL}"
+	done
 
-  echo "${CYAN}${BOLD}Do not commit fdescribe, fcontext or fit${NORMAL}"
-  exit 1
+	echo "${CYAN}${BOLD}Do not commit fdescribe, fcontext or fit${NORMAL}"
+exit 1
 else
-  for FILE in ${RESULT[@]}
-  do
-    sed -i '' -e 's/fit(/it(/g;' -e 's/fcontext(/context(/g;' -e 's/fdescribe(/describe(/g;' $FILE
+	for FILE in ${RESULT[@]}
+	do
+		sed -i '' -e 's/fit(/it(/g;' -e 's/fcontext(/context(/g;' -e 's/fdescribe(/describe(/g;' $FILE
 
-    FILE=$(basename "$FILE")
-    echo "${GREEN}$FILE${NORMAL}"
-  done
+		FILE=$(basename "$FILE")
+		echo "${GREEN}$FILE${NORMAL}"
+	done
 
-  echo "${GREEN}${BOLD}Removed all focuses${NORMAL}"
-  exit 0
+	echo "${GREEN}${BOLD}Removed all focuses${NORMAL}"
+	exit 0
 fi
