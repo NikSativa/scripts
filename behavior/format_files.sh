663b207a4a938b9490256bc02e67a8166dc9bf44
Nikita Konopelko <nik.sativa@gmail.com>
2023-01-24 22:57:17 +0200

    swiftformat and sort proj

---
+++ behavior/format_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# add swiftlint swiftformat to PATH
+export PATH=/usr/local/bin:$PATH
+
+script_full_path=$(dirname "$0")
+src_root_path=$(PWD)
+
+if [[ $src_root_path == *".swiftpm/xcode"* ]]; then
+  src_root_path=$src_root_path/../../
+fi
+
+if ! which swiftformat >/dev/null; then
+  brew install swiftformat
+fi
+
+if which swiftformat >/dev/null; then
+  swiftformat_path="$src_root_path/.swiftformat"
+  if [ -f $swiftformat_path ]; then
+    swiftformat --config $swiftformat_path $src_root_path
+  else
+    swiftformat --config "$script_full_path/default.swiftformat" $src_root_path
+  fi
+else
+  osascript -e "display notification \"warning: SwiftFormat not installed, run 'brew install SwiftFormat' or 'arch -x86_64 brew install swiftformat'\""
+  exit 1
+fi
+
+osascript -e "display notification \"files formated\""
+exit 0
--- behavior/format_xproj.sh
+++ behavior/format_xproj.sh
@@ -10,24 +10,6 @@ if [[ $src_root_path == *".swiftpm/xcode"* ]]; then
   src_root_path=$src_root_path/../../
 fi
 
-# 1. swiftformat
-if ! which swiftformat >/dev/null; then
-  brew install swiftformat
-fi
-
-if which swiftformat >/dev/null; then
-  swiftformat_path="$src_root_path/.swiftformat"
-  if [ -f $swiftformat_path ]; then
-    swiftformat --config $swiftformat_path $src_root_path
-  else
-    swiftformat --config "$script_full_path/default.swiftformat" $src_root_path
-  fi
-else
-  osascript -e "display notification \"warning: SwiftFormat not installed, run 'brew install SwiftFormat'\""
-  exit 1
-fi
-
-# 2. sort_Xcode_project_file
 shell_sort_path=$script_full_path/sort_Xcode_project_file
 if ! [ -f $shell_sort_path ]; then
   sh $script_full_path/download_sort_Xcode_project_file.sh
@@ -42,5 +24,5 @@ for project_path in $(find $src_root_path -name "*.xcodeproj" -not -path "$src_r
   fi
 done
 
-osascript -e "display notification \"jobs done\""
+osascript -e "display notification \"xpoj sorted\""
 exit 0
